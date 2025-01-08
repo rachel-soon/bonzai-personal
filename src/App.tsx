face5c3bb6890297e46ef51db5cb1a057a68f326
rachel-soon <rachel_may0106@hotmail.com>
2025-01-09 00:07:43 +0800

    feat: implement Auth0 react

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import LoginButton from "./LoginButton";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold text-red-500">Hello world!</h1>
+
+      <LoginButton />
     </>
   );
 }
